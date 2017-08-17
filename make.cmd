a1c4a9b85e7ddf17a9a086c0cae7dd16464a9714
ion <ionix@protonmail.com>
2017-08-17 22:30:24 +0200

    just to be sure...

--- make.cmd
+++ make.cmd
@@ -1,4 +1,4 @@
-del /q obj\*
-del /q bin\*
+del /q obj\*.obj
+del /q bin\*.exe
 cl /Fo.\obj\ -c src/*.cpp /I include -DUNICODE -D_UNICODE
 link /OUT:bin/opengl.exe /libpath:lib obj/*.obj kernel32.lib user32.lib gdi32.lib opengl32.lib minicrt.lib
